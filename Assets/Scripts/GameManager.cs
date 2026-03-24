612341b5308dd7d0930ea8bb065e42cb7bf2b437
TheKappaOne <79160101+TheKappaOne@users.noreply.github.com>
2026-03-24 12:56:11 -0400

    s

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
 
         if (scoreText != null)
             scoreText.text = "Score: " + score;
-    }
+    }   
     void GameOver()
     {
         gameOver = true;
